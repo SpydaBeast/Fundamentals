801a9f5a047228c38cf5123b35fea54a8dc1f351
brien <brien@artofmars.org>
2018-04-19 21:30:28 -0700

    Created data object for component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-events',
-  templateUrl: './events-list.component.html',
-  styleUrls: ['./events-list.component.css']
+  selector: 'events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr>
+    <div class="well hoverwell thumbnail">
+      <h2>{{event.name}}</h2>
+      <div> Date: {{event.date}}</div>
+      <div> Time: {{event.time}}</div>
+      <div> Price: \${{event.registration}}</div>
+    </div>
+  </div>
+  `
 })
 export class EventsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  event = {
+    id: 1,
+    name: 'Jiu Jitsu World Championship',
+    date: '1/1/2020',
+    time: '9:00 am',
+    registration: 165.00,
+    imageUrl: '',
+    location:{
+      address: ' 555 example road',
+      city: 'London',
+      country: "United States"
+    }
+
   }
 
 }
