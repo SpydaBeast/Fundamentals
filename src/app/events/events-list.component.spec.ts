694a1b88d032c08de967057cb341cf0a46730afe
brien <brien@artofmars.org>
2018-04-21 21:44:20 -0700

    fix broken reference

--- src/app/events/events-list.component.spec.ts
+++ src/app/events/events-list.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { EventsComponent } from './events.component';
+import { EventsComponent } from './events-list.component';
 
 describe('EventsComponent', () => {
   let component: EventsComponent;
