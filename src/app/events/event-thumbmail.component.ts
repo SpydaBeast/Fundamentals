2756b3976c02ee27f1e39b4522e63520464d56e2
brien <brien@artofmars.org>
2018-04-22 23:30:18 -0700

    added data object

--- src/app/events/event-thumbmail.component.ts
+++ src/app/events/event-thumbmail.component.ts
@@ -1,30 +1,29 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
-    selector: 'event-thumbnail',
-    template:`
+  selector: 'event-thumbnail',
+  template: `
     <div>
       <hr>
       <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div> Date: {{event.date}}</div>
         <div> Time: {{event.time}}</div>
-        <div> Price: \${{event.registration}}</div>
+        <div> Price: {{event.price}}</div>
         <span>Location: {{event.location.address}}</span>
         <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
       </div>
         </div>
-      
     `,
-    styles:[`
+  styles: [`
       .pad-left { margin-left: 10px; }
       .well div { color: auto; }
       `
-]
+  ]
 })
-    export class EventThumbnailComponent {
-       @Input() event:any
+export class EventThumbnailComponent {
+  @Input() event: any
 
 
-    }
+}
 
